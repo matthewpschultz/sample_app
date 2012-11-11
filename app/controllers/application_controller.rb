bddaa04d62ad91292acbf847b439bc1849ae5dba
matthewpschultz <matthewpschultz@yahoo.com>
2012-11-11 17:38:34 -0500

    still screwing with heroku problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :set_no_cache
+  private
+  def set_no_cache
+    headers['Cache-Control'] = 'no-cache'
+  end
 end
